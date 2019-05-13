b1df2466b95fb1d8e2a05fe98b1c664596c8e2a4
Paul Holmes <paulholmes1993@gmail.com>
2019-05-13 00:15:03 +0000

    Automatically backed up by Learn

---
+++ prime.rb
@@ -0,0 +1,15 @@
+def prime?(number)
+  range = (2..(number - 1)).to_a
+  check = true
+  if number < 2
+    check = false
+  else
+    range.each do |x|
+      if number % x == 0
+        check = false
+        break
+      end
+    end
+  end
+   puts check
+end
