a798d1bd4e617927e001eca68f257ce7802bb96a
Paul Holmes <paulholmes1993@gmail.com>
2019-05-13 00:18:29 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,15 +1,11 @@
 def prime?(number)
-  range = (2..(number - 1)).to_a
-  check = true
-  if number < 2
-    check = false
-  else
-    range.each do |x|
-      if number % x == 0
-        check = false
-        break
-      end
+  start = 2
+  if number > 1
+    range = (start..number-1).to_a
+    range.none? do |num_to_test| 
+      number % num_to_test == 0
     end
+  else
+    false
   end
-   puts check
 end
